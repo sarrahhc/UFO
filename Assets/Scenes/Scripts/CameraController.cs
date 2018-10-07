7e1c4c163806f36a3c795d99b4623d983351be67
sarrahhc <42785057+sarrahhc@users.noreply.github.com>
2018-10-07 12:27:28 -0400

    UFO Game

---
+++ Assets/Scenes/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;      
+    private Vector3 offset;         
+
+
+    void Start () 
+    {
+
+        offset = transform.position - player.transform.position;
+    }
+    
+
+    void LateUpdate () 
+    {
+
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scenes/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Rotator : MonoBehaviour
+{
+
+    void Update()
+    {
+
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+}
